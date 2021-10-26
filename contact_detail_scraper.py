11474fcbc2b053a00f57d40b69ded5b5a2eba406
SanketDobaria <sanket.tagline@gmail.com>
2021-10-26 09:40:58 +0530

    Added headers for request method

--- contact_detail_scraper.py
+++ contact_detail_scraper.py
@@ -2,26 +2,36 @@ import requests
 import bs4
 import re
 
+
 def get_email(soup):
     try:
         mailtos = soup.select('a[href^=mailto]')
+        emailList = []
         for i in mailtos:
-            href=i['href']
+            href = i['href']
             try:
                 x, mail = href.split(':')
             except ValueError:
                 break
-            return mail
+            emailList.append(mail)
+        return emailList
     except:
         pass
 
+
 urls = ["https://www.significantinfotech.com/about-us/",
-        "https://taglineinfotech.com/",]
+        "https://taglineinfotech.com/", ]
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
 
 detail_list = []
+
 for url in urls:
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         soup = bs4.BeautifulSoup(response.text, 'html.parser')
     except:
         continue
