3ae4a4fd3f7264758b2194ba24188bafba131a8c
Pritesh <pritesh.tagline@gmail.com>
2021-11-01 12:04:50 +0530

    updated get_email function

--- contact_detail_scraper.py
+++ contact_detail_scraper.py
@@ -3,6 +3,14 @@ import bs4
 import re
 
 
+def email_list_filter(emailList):
+    if len(emailList) > 1:
+            for email in emailList:
+                address = email.split('@')[0]
+                if address.lower() == 'hr':
+                    emailList.remove(email)
+    return emailList
+
 def get_email(soup):
     try:
         mailtos = soup.select('a[href^=mailto]')
@@ -23,23 +31,78 @@ def get_email(soup):
                 try:
                     x, mail = href.split(':')
                 except ValueError:
-                    break
+                    continue
                 emailList.append(mail)
+        
+        if len(emailList) == 0:
+            try:
+                footer = str(soup.select('footer'))
+                emailList = re.findall(r'([a-zA-Z0-9._-]+@[a-zA-Z._-]+\.[a-zA-Z_-]+)', footer)
+                emailList = email_list_filter(emailList)
                 return emailList
+            except:
+                pass
+        else:
+            emailList = email_list_filter(emailList)
+            return emailList
     except:
         pass
 
-    try:
-        footer = str(soup.select('footer'))
-        emailList = re.findall(r'([a-zA-Z0-9._-]+@[a-zA-Z._-]+\.[a-zA-Z_-]+)', footer)
-        return emailList
-    except:
-        pass
+# def get_contact_number(soup):
+#     try:
+#         contact_number = soup.select_one('a[href]^=tel')
+#         href = contact_number['href']
+#         try:
+#             x, contact_number = href.split(':')
+#             return contact_number
+#         except ValueError:
+#             pass
+#     except:
+#         pass
+#     try:
+#         contact_number = soup.select_one('a[href]^=callto')
+#         href = contact_number['href']
+#         try:
+#             x, contact_number = href.split(':')
+#             return contact_number
+#         except ValueError:
+#             pass
+#     except:
+#         pass
+#     return None
 
+# def get_details(soup):
+#     try:
+#         mailtos = soup.select('a[href^=mailto]')
+#         emailList = []
+#         for mailto in mailtos:
+#             content = mailto.parent
+#             if "sales" in str(content).lower():
+#                 href = mailto['href']
+#                 try:
+#                     x, mail = href.split(':')
+#                 except ValueError:
+#                     break
+#                 emailList.append(mail)
+#                 contact_info = get_contact_number(content)
+#                 return emailList,contact_info
+#             else:
+#                 href = mailto['href']
+#                 try:
+#                     x, mail = href.split(':')
+#                 except ValueError:
+#                     break
+#                 emailList.append(mail)
+#         emailList = email_list_filter(emailList)
+#         return emailList
+#     except:
+#         pass
 
-urls = ["https://www.significantinfotech.com",
+urls = [
+        "https://www.significantinfotech.com",
         "https://taglineinfotech.com/",
-        "https://www.c-sharpcorner.com/article/how-to-validate-an-email-address-in-python/",]
+        "https://www.c-sharpcorner.com/article/how-to-validate-an-email-address-in-python/",
+    ]
 
 
 headers = {
